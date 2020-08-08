b7d1470b9f4f6beb2a2b2c4e7fbbcfd632e21b31
Duckfan77 <cwenthin@yahoo.com>
2020-08-08 17:21:35 -0500

    Add Shading of the sphere based on normals

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
 #![allow(dead_code)]
 
 use std::io::{Write, stderr, stdout};
+use std::f64;
 mod vec3;
 mod color;
 mod ray;
 
 use vec3::*;
 
-fn hit_sphere(center: &Point, radius: f64, r: &ray::Ray) -> bool{
+fn hit_sphere(center: &Point, radius: f64, r: &ray::Ray) -> f64{
     let oc: Vec3 = r.origin() - *center;
     let a = Vec3::dot(r.direction(), r.direction());
     let b = 2.0 * Vec3::dot(oc, r.direction());
     let c = Vec3::dot(oc, oc) - radius*radius;
     let discriminant = b*b - 4.0*a*c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0
+    }else{
+        return (-b - f64::sqrt(discriminant)) / (2.0*a)
+    }
 }
 
 fn ray_color(r: &ray::Ray) -> Color{
-    if hit_sphere(&Point::new(0.0, 0.0, -1.0), 0.5, r){
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Point::new(0.0, 0.0, -1.0), 0.5, r);
+    if t>0.0 {
+        //assume Normals are unit vectors, not enforced
+        let N = Vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(N.x()+1.0, N.y()+1.0, N.z()+1.0);
     }
+
     let unit_direction = Vec3::unit_vector(r.direction());
     let t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*Color::new(1.0, 1.0, 1.0) + t*Color::new(0.5, 0.7, 1.0);

bea877d6aea8f4e08e023a3f188d115b7f071674
Duckfan77 <cwenthin@yahoo.com>
2020-08-08 17:11:15 -0500

    Add check from sphere intersection
    Added a system to check if a ray intersects a sphere, when finding the color of the ray.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,19 @@ mod ray;
 
 use vec3::*;
 
+fn hit_sphere(center: &Point, radius: f64, r: &ray::Ray) -> bool{
+    let oc: Vec3 = r.origin() - *center;
+    let a = Vec3::dot(r.direction(), r.direction());
+    let b = 2.0 * Vec3::dot(oc, r.direction());
+    let c = Vec3::dot(oc, oc) - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+    discriminant > 0.0
+}
+
 fn ray_color(r: &ray::Ray) -> Color{
+    if hit_sphere(&Point::new(0.0, 0.0, -1.0), 0.5, r){
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = Vec3::unit_vector(r.direction());
     let t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*Color::new(1.0, 1.0, 1.0) + t*Color::new(0.5, 0.7, 1.0);

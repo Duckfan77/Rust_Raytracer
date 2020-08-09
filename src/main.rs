b233b3ceb4a527579afa34b20545ab017fd1d530
Duckfan77 <cwenthin@yahoo.com>
2020-08-09 00:15:31 -0500

    Implement multiple samples per pixel with Camera
    Created a camera module, to remove the camera ray calculations from main, and implemented a multisample per pixel render system.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod hittable;
 mod sphere;
 mod hittable_list;
 mod util;
+mod camera;
 
 use vec3::*;
 use util::*;
@@ -31,6 +32,7 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
+    let sample_per_pixel = 100;
 
     // World
     let mut world = hittable_list::HittableList {objects: Vec::with_capacity(10)};
@@ -38,14 +40,7 @@ fn main() {
     world.add(Rc::new(sphere::Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let cam = camera::Camera::new();
 
     //Render
     print!("P3\n{} {}\n255\n", image_width, image_height);
@@ -55,12 +50,18 @@ fn main() {
         stderr().flush().expect("Fail to flush stderr");
 
         for i in 0..image_width {
-            let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
-            let r = ray::Ray::new(&origin, &(lower_left_corner + u*horizontal + v*vertical - origin));
-            let pixel = ray_color(&r, &world);
+            let mut pixel = Color::new(0.0, 0.0, 0.0);
 
-            color::write_color(stdout(), pixel, 1);
+            for _ in 0..sample_per_pixel {
+                let u = (i as f64 + random_double()) / (image_width - 1) as f64;
+                let v = (j as f64 + random_double()) / (image_height - 1) as f64;
+
+                let r = cam.get_ray(u, v);
+
+                pixel += ray_color(&r, &world);
+            }
+
+            color::write_color(stdout(), pixel, sample_per_pixel);
         }
     }
 }

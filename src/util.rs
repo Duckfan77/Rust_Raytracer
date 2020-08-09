e71a090949e6b23e67403e932f843b6040b07434
Duckfan77 <cwenthin@yahoo.com>
2020-08-08 22:47:21 -0500

    Add random_double util functions
    Added two helper functions in util wrapping rand::thread_rng().gen_range to get random numbers in an arbitrary range, and on [0,1).

--- src/util.rs
+++ src/util.rs
@@ -11,3 +11,15 @@ pub fn degs_to_rads(deg: f64) -> f64 {
 pub fn rads_to_degs(rad: f64) -> f64 {
     rad * 180.0 / PI
 }
+
+use rand::Rng;
+
+pub fn random_double() -> f64 {
+    // Gets a random double in [0,1)
+    rand::thread_rng().gen_range(0.0, 1.0)
+}
+
+// Gets a random double in [min,max). panics if min >= max
+pub fn random_double_range(min: f64, max: f64) -> f64 {
+    rand::thread_rng().gen_range(min, max)
+}

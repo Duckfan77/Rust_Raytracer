2c9272388654c775432701c40a943758dffc967c
Duckfan77 <cwenthin@yahoo.com>
2020-08-17 01:12:53 -0500

    Create Final Scene

--- src/main.rs
+++ src/main.rs
@@ -188,6 +188,67 @@ fn cornell_smoke() -> hittable_list::HittableList {
     return objects
 }
 
+fn final_scene() -> hittable_list::HittableList {
+    let mut boxes1 = hittable_list::HittableList {objects: Vec::with_capacity(10)};
+    let ground: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.48, 0.83, 0.53)));
+
+    let boxes_per_side = 20;
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64*w;
+            let z0 = -1000.0 + j as f64*w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = random_double_range(1.0, 101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(Rc::new(boxes::Box::new(&Point::new(x0, y0, z0), &Point::new(x1, y1, z1), Rc::clone(&ground))));
+        }
+    }
+
+    let mut objects = hittable_list::HittableList::new(Rc::new(bvh::BvhNode::new_l(&mut boxes1, 0.0, 1.0)));
+
+    let light: Rc<dyn Material> = Rc::new(DiffuseLight::new(Color::new(7.0, 7.0, 7.0)));
+    objects.add(Rc::new(aarect::XZRect::new(123.0, 423.0, 147.0, 412.0, 554.0, Rc::clone(&light))));
+
+    let center1 = Point::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let mov_sph_mat: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.1)));
+    objects.add(Rc::new(moving_sphere::MovingSphere::new(center1, center2, 0.0, 1.0, 50.0, Rc::clone(&mov_sph_mat))));
+
+    objects.add(Rc::new(sphere::Sphere::new(Point::new(260.0, 150.0, 45.0), 50.0, Rc::new(Dialectric::new(1.5)))));
+    objects.add(Rc::new(sphere::Sphere::new(
+        Point::new(0.0, 150.0, 145.0), 50.0, Rc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 10.0))
+    )));
+
+    let mut boundary: Rc<dyn Hittable> = Rc::new(sphere::Sphere::new(Point::new(360.0, 150.0, 145.0), 70.0, Rc::new(Dialectric::new(1.5))));
+    objects.add(Rc::clone(&boundary));
+    objects.add(Rc::new(constant_medium::ConstantMedium::new(Rc::clone(&boundary), 0.2, Color::new(0.2, 0.4, 0.9))));
+    boundary = Rc::new(sphere::Sphere::new(Point::new(0.0, 0.0, 0.0), 5000.0, Rc::new(Dialectric::new(1.5))));
+    objects.add(Rc::new(constant_medium::ConstantMedium::new(Rc::clone(&boundary), 0.0001, Color::new(1.0, 1.0, 1.0))));
+
+    let emat: Rc<dyn Material> = Rc::new(Lambertian::new_txtr(&(Rc::new(ImageTexture::new("earthmap.jpg")) as Rc<dyn Texture>)));
+    objects.add(Rc::new(sphere::Sphere::new(Point::new(400.0, 200.0, 400.0), 100.0, emat)));
+    let pertext: Rc<dyn Texture> = Rc::new(MarbleNoiseTexture::new_sc(0.1));
+    objects.add(Rc::new(sphere::Sphere::new(Point::new(220.0, 280.0, 300.0), 80.0, Rc::new(Lambertian::new_txtr(&pertext)))));
+
+    let mut boxes2 = hittable_list::HittableList {objects: Vec::with_capacity(10)};
+    let white: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for _ in 0..ns {
+        boxes2.add(Rc::new(sphere::Sphere::new(random_range(0.0, 165.0), 10.0, Rc::clone(&white))));
+    }
+
+    objects.add(Rc::new(Translate::new(
+        Rc::new(RotateY::new(
+            Rc::new(bvh::BvhNode::new_l(&mut boxes2, 0.0, 1.0)), 15.0)),
+        &Vec3::new(-100.0, 270.0, 395.0)
+    )));
+
+    return objects
+}
+
 fn ray_color(r: &ray::Ray, background: &Color, world: &dyn hittable::Hittable, depth: u32) -> Color{
     let mut rec = hittable::HitRecord::new();
 
@@ -283,7 +344,7 @@ fn main() {
             vfov = 40.0;
         }
 
-        7 | _ => {
+        7 => {
             world = cornell_smoke();
             aspect_ratio = 1.0;
             image_width = 600;
@@ -291,6 +352,17 @@ fn main() {
             lookfrom = Point::new(278.0, 278.0, -800.0);
             lookat = Point::new(278.0, 278.0, 0.0);
             vfov = 40.0;
+        }
+
+        8 | _ => {
+            world = final_scene();
+            aspect_ratio = 1.0;
+            image_width = 800;
+            sample_per_pixel = 10000;
+            background = Color::new(0.0, 0.0, 0.0);
+            lookfrom = Point::new(478.0, 278.0, -600.0);
+            lookat = Point::new(278.0, 278.0, 0.0);
+            vfov = 400.0;
         }
     }
 

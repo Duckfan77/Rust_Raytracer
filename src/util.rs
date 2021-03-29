eb0c759849dba7affaa3d71ad8e8a03ef16114e4
Melissa <77photon@gmail.com>
2021-03-28 20:28:23 -0500

    Update Library Versions
    Updated library versions, and updated util to use the new random get_range syntax

--- src/util.rs
+++ src/util.rs
@@ -17,12 +17,12 @@ use rand::Rng;
 
 pub fn random_double() -> f64 {
     // Gets a random double in [0,1)
-    rand::thread_rng().gen_range(0.0, 1.0)
+    rand::thread_rng().gen_range(0.0..1.0)
 }
 
 // Gets a random double in [min,max). panics if min >= max
 pub fn random_double_range(min: f64, max: f64) -> f64 {
-    rand::thread_rng().gen_range(min, max)
+    rand::thread_rng().gen_range(min..max)
 }
 
 pub fn random_int_range(min: i32, max: i32) -> i32 {

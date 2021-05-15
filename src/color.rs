518b483a045655646b9780a1deca6c061b8782e7
Melissa <77photon@gmail.com>
2021-05-15 18:38:42 -0500

    Add RGB ImageBuffer OutputFunctions
    added three functions for RGB ImageBuffers, for 8, 16, and 32 bits per
    pixel.

--- src/color.rs
+++ src/color.rs
@@ -2,6 +2,7 @@ use crate::vec3;
 use std::io;
 use std::fmt;
 use crate::util::*;
+use image::{Rgb, ImageBuffer, RgbImage};
 
 fn convert_pixel(pixel: vec3::Color, sample_count: u32) -> (f64, f64, f64){
     let mut r = pixel.x();
@@ -29,4 +30,39 @@ pub fn write_pixel_str_ppm<T: fmt::Write>(out: &mut T, pixel: vec3::Color, sampl
     let (r,g,b) = convert_pixel(pixel, sample_count);
 
     let _ = write!(out, "{} {} {}\n", (256.0 * clamp(r, 0.0, 0.999)) as u32, (256.0 * clamp(g, 0.0, 0.999)) as u32, (256.0 * clamp(b, 0.0, 0.999)) as u32);
+}
+pub fn write_pixel_img_8bpp(x: u32, y: u32, pixel: vec3::Color, sample_count: u32, img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) {
+    let (r,g,b) = convert_pixel(pixel, sample_count);
+
+    let max_color = std::u8::MAX as f64 + 1.0;
+
+    img.put_pixel(x, y, Rgb(
+        [(max_color * clamp(r, 0.0, 0.999)) as u8,
+         (max_color * clamp(g, 0.0, 0.999)) as u8,
+         (max_color * clamp(b, 0.0, 0.999)) as u8]
+    ))
+}
+
+pub fn write_pixel_img_16bpp(x: u32, y: u32, pixel: vec3::Color, sample_count: u32, img: &mut ImageBuffer<Rgb<u16>, Vec<u16>>) {
+    let (r,g,b) = convert_pixel(pixel, sample_count);
+
+    let max_color = std::u16::MAX as f64 + 1.0;
+
+    img.put_pixel(x, y, Rgb(
+        [(max_color * clamp(r, 0.0, 0.999)) as u16,
+         (max_color * clamp(g, 0.0, 0.999)) as u16,
+         (max_color * clamp(b, 0.0, 0.999)) as u16]
+    ))
+}
+
+pub fn write_pixel_img_32bpp(x: u32, y: u32, pixel: vec3::Color, sample_count: u32, img: &mut ImageBuffer<Rgb<u32>, Vec<u32>>) {
+    let (r,g,b) = convert_pixel(pixel, sample_count);
+
+    let max_color = std::u32::MAX as f64 + 1.0;
+
+    img.put_pixel(x, y, Rgb(
+        [(max_color * clamp(r, 0.0, 0.999)) as u32,
+         (max_color * clamp(g, 0.0, 0.999)) as u32,
+         (max_color * clamp(b, 0.0, 0.999)) as u32]
+    ))
 }
